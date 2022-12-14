e06b99d8ad4c1bbbc0b89f7c469f41c0a7cefde1
Freddyede <f.patouillard@it-students.fr>
2022-12-14 18:50:39 +0100

    first commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @name HomeController
+ * @version 1.0.0
+ * @extends AbstractController
+ * @uses  \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
+ * @uses  \Symfony\Component\HttpFoundation\Response
+ * @uses  \Symfony\Component\Routing\Annotation\Route
+ */
+// section Home
+class HomeController extends AbstractController {
+    /**
+     * Description : Return homepage website
+     * @version 1.0.0
+     * @author Franck Patouillard<patouillardfranck3@gmail.com>
+     * @return Response
+     * @method index
+     */
+    #[Route('/', name: 'app_home')]
+    public function index(): Response {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);w
+    }
+}
